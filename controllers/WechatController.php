32a7cead94d481e071670e0b4f78fe315d3a8fbe
chentulin <chentulinys@163.com>
2019-11-25 13:42:51 +0800

    191125

---
+++ controllers/WechatController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class WechatController extends Controller
+{
+    public function actionTest()
+    {
+        var_dump(111111111111);
+    }
+}
