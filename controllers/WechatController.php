3d0494086c6143cd6608c76a97d7c8479fa38962
chentulin <chentulinys@163.com>
2019-11-27 15:57:58 +0800

    191127

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -12,4 +12,73 @@ class WechatController extends Controller
     {
         var_dump(111111111111);
     }
+
+    public function valid(){
+        //获取随机字符串
+        $echoStr = input("echostr");
+
+        if($echoStr){
+            // 验证接口的有效性，由于接口有效性的验证必定会传递echostr 参数
+            if($this ->checkSignature()){
+                echo $echoStr;
+                exit;
+            }
+        }else{
+            $this->responseMsg();
+        }
+    }
+
+    protected function checkSignature()
+    {
+        $signature = input("signature");
+        $timestamp = input("timestamp");//时间戳
+        $nonce =input("nonce");//随机数
+        $token = "weixin";  //token值，必须和你设置的一样
+        $tmpArr =array($token,$timestamp,$nonce);
+        sort($tmpArr,SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr =sha1($tmpStr);
+        if($tmpStr == $signature){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = file_get_contents('php://input');
+        //extract post data
+        if (!empty($postStr)){
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = '<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            <FuncFlag>0</FuncFlag>
+                            </xml>';
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+        }else {
+            echo "";
+            exit;
+        }
+    }
 }
