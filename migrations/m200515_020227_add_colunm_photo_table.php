830732687d1a364818b7c6372dce59d6eabde314
chentulin <chentulinys@163.com>
2020-05-15 17:38:10 +0800

    增加新文件

---
+++ migrations/m200515_020227_add_colunm_photo_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200515_020227_add_colunm_photo_table
+ */
+class m200515_020227_add_colunm_photo_table extends Migration
+{
+    public $tableName = '{{%photo}}';
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn($this->tableName, 'thumb_img' , $this->string()->notNull()->defaultValue('')->comment('生成缩略图'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn($this->tableName ,'thumb_img');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200515_020227_add_colunm_photo_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
