5a34b723bf07ce9cdac2e74e5962408a740314f9
yyctl <527716454@qq.com>
2020-05-20 13:38:14 +0800

    feat : 添加公众号测试

--- config/params.php
+++ config/params.php
@@ -16,8 +16,8 @@ return [
     ],
     'testWeChat'  => [
         'debug'         => false,
-        'app_id'        => 'wxc439cbfe9ee8140e', // AppID
-        'secret'        => 'b2bf1b59f797e4c0cea4c44b4bfe81f9', // AppSecret
+        'app_id'        => 'wxa778c78314461526', // AppID
+        'secret'        => '85717a0434fd11a199590620e0be5648', // AppSecret
         'token'         => 'chentulin', // Token
         'response_type' => 'array',//返回类型
     ]
