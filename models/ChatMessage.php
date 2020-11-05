f71ac8462ea00aa2a84d5f63ad97b917afcdb78d
shy <chentulin@163.com>
2020-11-05 20:36:22 +0800

    'fix:添加少更类'

---
+++ models/ChatMessage.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "chat_message".
+ *
+ * @property int $id id
+ * @property string $openid openid
+ * @property int $custom_id 客服id
+ * @property string $created_time 创建时间
+ * @property string $type 记录类型
+ * @property int $is_read 是否已读,1/未读,2/已读
+ * @property string $content 聊天内容
+ */
+class ChatMessage extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'chat_message';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['custom_id', 'is_read'], 'integer'],
+            [['created_time'], 'safe'],
+            [['content'], 'required'],
+            [['content'], 'string'],
+            [['openid'], 'string', 'max' => 300],
+            [['type'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'openid' => 'Openid',
+            'custom_id' => 'Custom ID',
+            'created_time' => 'Created Time',
+            'type' => 'Type',
+            'is_read' => 'Is Read',
+            'content' => 'Content',
+        ];
+    }
+}
