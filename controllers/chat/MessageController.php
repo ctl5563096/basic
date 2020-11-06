b1231d71d245ca9227592cd2f78dce9358000bb9
shy <chentulin@163.com>
2020-11-06 18:07:12 +0800

    'feat:新增微信消息回访'

--- controllers/chat/MessageController.php
+++ controllers/chat/MessageController.php
@@ -3,7 +3,10 @@
 
 namespace app\controllers\chat;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\Response;
+use app\service\WeChatService;
 
 /**
  * 客服聊天控制器
@@ -17,6 +20,7 @@ class MessageController extends Controller
      * @var bool 关闭yii2自带布局
      */
     public $layout = false;
+
     /**
      * 渲染聊天页面
      *
@@ -26,5 +30,35 @@ class MessageController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    /**
+     * Notes: 发送消息
+     *
+     * Author: chentulin
+     * DateTime: 2020/11/6 11:16
+     * E-MAIL: <chentulinys@163.com>
+     */
+    public function actionSend()
+    {
+        $request          = Yii::$app->request;
+        $response         = Yii::$app->response;
+        $response->format = Response::FORMAT_JSON;
+        // 获取openId和customId
+        $openid   = $request->post('openid');
+        $customId = $request->post('customId');
+        $content  = $request->post('content');
+        if (!$openid || !$customId || !$content){
+            $response->data = ['code' => '400' , 'msg' => '缺少参数'];
+        }else{
+            // 获取发送内容
+            $weChatServiceRes = WeChatService::sendMessage($openid,(int)$customId,$content);
+                if ($weChatServiceRes){
+                    $response->data = ['code' => '200' , 'msg' => '发送成功'];
+                }else{
+                    $response->data = ['code' => '400' , 'msg' => '发送失败'];
+            }
+        }
+        return $response;
     }
 }
---
+++ service/WeChatService.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+
+namespace app\service;
+
+use app\models\ChatMessage;
+use EasyWeChat\Factory;
+use EasyWeChat\Kernel\Exceptions\InvalidArgumentException;
+use EasyWeChat\Kernel\Exceptions\InvalidConfigException;
+use EasyWeChat\Kernel\Exceptions\RuntimeException;
+use EasyWeChat\Kernel\Messages\Text;
+use Yii;
+
+/**
+ * 微信接口服务层
+ *
+ * Class WeChatService
+ * @package app\service
+ */
+class WeChatService extends BaseService
+{
+    // 微信客户端实例化对象
+    public $app;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // 这里获取微信客户端实例化对象
+        $this->app = Factory::officialAccount(Yii::$app->params['testWeChat']);
+    }
+
+    /**
+     * Notes: 获取微信客户端实例化对象
+     *
+     * Author: chentulin
+     * DateTime: 2020/11/6 11:29
+     * E-MAIL: <chentulinys@163.com>
+     */
+    public function getInstance()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Notes: 发送消息
+     *
+     * Author: chentulin
+     * DateTime: 2020/11/6 11:30
+     * E-MAIL: <chentulinys@163.com>
+     * @param string $openid
+     * @param int $customId
+     * @param string $content
+     * @return bool
+     */
+    public static function sendMessage(string $openid, int $customId, string $content)
+    {
+        $object  = new self();
+        $message = new Text($content);
+        try {
+           $object->app->customer_service->message($message)->to($openid)->send();
+        } catch (InvalidArgumentException $e) {
+            Yii::info($e->getMessage().$openid);
+            return false;
+        } catch (InvalidConfigException $e) {
+            Yii::info($e->getMessage().$openid);
+            return false;
+        } catch (RuntimeException $e) {
+            Yii::info($e->getMessage().$openid);
+            return false;
+        }
+        // 保存客服聊天记录
+        $messageModel            = new ChatMessage();
+        $messageModel->content   = $content;
+        $messageModel->openid    = $openid;
+        $messageModel->is_read   = 2;
+        $messageModel->custom_id = $customId;
+        $messageModel->type      = 'text';
+        $messageModel->save();
+        return true;
+    }
+}
